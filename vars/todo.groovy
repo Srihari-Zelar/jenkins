36ee6c715d25fc146fa5697dd22cfe7e88e33c8e
Srihari-Zelar <82359435+Srihari-Zelar@users.noreply.github.com>
2021-07-22 15:49:56 +0530

    Update todo.groovy

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -1,7 +1,7 @@
 def call(Map params = [:]) {
     // Start Default Arguments
     def args = [
-            NEXUS_IP               : '172.31.4.34',
+            NEXUS_IP               : '172.31.4.238',
     ]
     args << params
 
