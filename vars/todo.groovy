08ef6c905328baf36557ed07f047898a862c35eb
Srihari <Srihari@zelarsoft.com>
2021-07-26 20:39:26 +0530

    update

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -27,6 +27,7 @@ def call(Map params = [:]) {
 
             stage('Build Code & Install Dependencies') {
                 steps {
+                    sh 'env'
                     script {
                         build = new nexus()
                         build.code_build("${APP_TYPE}", "${COMPONENT}")
@@ -54,8 +55,10 @@ def call(Map params = [:]) {
 
             stage('Deploy to Dev Env') {
               steps {
-
-                build job: 'Deployment Pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: '0.0.0')]
+                  script {
+                      get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+                      env.get_branch_exec=sh(returnStdout: true, script: get_branch)
+                build job: 'Deployment Pipeline', parameters: [string(name: 'ENV', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: "${get_branch_exec}" )]
               }
             }
   
