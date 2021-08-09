cfd28567f04ec0cb51ee32a0e7351a1cca5bfe7e
Srihari-Zelar <82359435+Srihari-Zelar@users.noreply.github.com>
2021-08-09 15:35:27 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,61 +1,40 @@
 folder('CI-Pipelines') {
-    displayName('CI Pipelines')
-    description('CI Pipelines')
+  displayName('CI Pipelines')
+  description('CI Pipelines')
 }
 
-def component = ["frontend", "login","todo","users"];
+def component = ["cart", "catalogue","user","payment","shipping","frontend"];
 
 def count=(component.size()-1)
 for (i in 0..count) {
-    def j=component[i]
-    pipelineJob("CI-Pipelines/${j}-ci") {
-        configure { flowdefinition ->
-            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-                'triggers' {
-                    'hudson.triggers.SCMTrigger' {
-                        'spec'('*/2 * * * 1-5')
-                        'ignorePostCommitHooks'(false)
-                    }
-                }
-            }
-            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                    'userRemoteConfigs' {
-                        'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/Srihari-Zelar/'+j+'.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-                        }
-                    }
-                    'branches' {
-                        'hudson.plugins.git.BranchSpec' {
-                            'name'('*/tags/*')
-                        }
-                    }
-                }
-                'scriptPath'('Jenkinsfile')                
-                'lightweight'(true)
-            }
-        }
-    }
-}
-
-pipelineJob("Deployment Pipeline") {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/Srihari-Zelar/jenkins.git')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('main')
+  def j=component[i]
+  pipelineJob("CI-Pipelines/${j}-ci") {
+    configure { flowdefinition ->
+      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+        'triggers' {
+          'hudson.triggers.SCMTrigger' {
+            'spec'('*/2 * * * 1-5')
+            'ignorePostCommitHooks'(false)
           }
         }
       }
-      'scriptPath'('Jenkinsfile-Docker')
-      'lightweight'(true)
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/Srihari-Zelar/'+j+'.git')
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile-Docker')
+        'lightweight'(true)
+      }
     }
   }
 }
